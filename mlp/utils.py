8129e25da9ffb40cc82fab435026c8a2d77c4673
Luke <luke@clashproject.com>
2019-09-21 19:01:42 +0200

    Add list of valid characters

--- mlp/utils.py
+++ mlp/utils.py
@@ -21,3 +21,17 @@ def readDataset(fileName):
     txt = f.read()
   return txt
 
+def getListFromChar(char, chardict, classes):
+  num = chardict[char]
+  return [0]*num+[1]+[0]*(classes-1-num)
+
+def getCharacters(indexIn, classes):
+  import string
+  chars = string.ascii_lowercase+'.," '
+  charDict = {chars[i]: i for i in range(len(chars))}
+  if indexIn:
+    charDictList = {chars[i]: 1-i/classes/2 for i in range(len(chars))}
+  else:
+    charDictList = {chars[i]: getListFromChar(chars[i], charDict, classes) for i in range(len(chars))}
+  return chars, charDict, charDictList
+

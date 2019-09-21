90ec41d5b1a74ed7bffb55bbce46766acfdaa14e
Luke <luke@clashproject.com>
2019-09-21 19:01:08 +0200

    Add dataset+model readers

--- mlp/utils.py
+++ mlp/utils.py
@@ -10,3 +10,14 @@ def getPreviousWeightsFromGDrive(weightFolderName):
   import os
   os.system(''.join(['cp -r "/content/gdrive/My Drive/',weightFolderName,'" .']))
 
+def getLatestModelName(weightFolderName):
+  import os
+  all_files = [(name, os.path.getmtime(''.join(['.',weightFolderName,'/',name]))) for name in os.listdir(''.join(['.',weightFolderName,'/']))]
+  latest_uploaded_file = sorted(all_files, key=lambda x: -x[1])[0][0]
+  return './MLP_Weights/'+latest_uploaded_file
+
+def readDataset(fileName):
+  with open(fileName,'r') as f:
+    txt = f.read()
+  return txt
+

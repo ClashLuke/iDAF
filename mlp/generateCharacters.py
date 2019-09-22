7ba3095436b74f719498932aaed18b3506104e1d
Luke <luke@clashproject.com>
2019-09-22 18:27:56 +0200

    Use model from callback instead of passing it around

--- mlp/generateCharacters.py
+++ mlp/generateCharacters.py
@@ -3,41 +3,40 @@ import numpy as np
 import itertools
 
 class generateChars():
-  def __init__(self, classes, inputs, inputString, outCharCount, outputs, chars, charDictList, model):
+  def __init__(self, classes, inputs, inputString, outCharCount, outputs, chars, charDictList):
     self.classes = classes
     self.inputs = inputs
     self.outputs = outputs
     self.chars = chars
-    self.model = model
     self.charDictList = charDictList
     self.inputString = inputString
     self.outCharCount = outCharCount
 
-  def genKey(self, inp):
+  def genKey(self, inp, model):
     topred = np.zeros((1,self.classes*self.inputs))
     topred[0][:] = inp[:]
     if self.outputs == 1:
-      pred = np.argmax(self.model.predict(topred)[0])
+      pred = np.argmax(model.predict(topred)[0])
       pred = [self.chars[pred]]
     else:
-      pred = self.model.predict(topred)[0]
+      pred = model.predict(topred)[0]
       pred = [np.argmax(p) for p in pred]
       pred = [self.chars[p] for p in pred]
     return pred
     #return CHARS[np.argmax(pred)]
 
-  def genRecurse(self, instr):
+  def genRecurse(self, instr, model):
     # initial input
     inp = list(itertools.chain.from_iterable(
         [self.charDictList[self.inputString[i]] for i in range(self.inputs)]
     ))
     for i in range(self.outCharCount):
-      res = self.genKey(inp[i*30*self.outputs:])
+      res = self.genKey(inp[i*30*self.outputs:], model)
       inp = inp+list(itertools.chain.from_iterable([self.charDictList[r] for r in res]))
     return inp
 
-  def genStr(self, instr):
-    recOut = self.genRecurse(instr)
+  def genStr(self, instr, model):
+    recOut = self.genRecurse(instr, model)
     out = ''.join(self.chars[np.argmax(recOut[i*self.classes:(i+1)*self.classes])] for i in range(self.outCharCount))
     return out
 
@@ -47,5 +46,5 @@ class GenerateCharsCallback(tf.keras.callbacks.Callback):
     self.inputString = inputString
     self.inputs = inputs
   def on_epoch_end(self, batch, logs = {}):
-    print(self.generateCharsInstance.genStr(self.inputString)[self.inputs])
+    print(self.generateCharsInstance.genStr(self.inputString, self.model)[self.inputs])
     return None

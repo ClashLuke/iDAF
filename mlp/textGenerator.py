1f3ef7b8c595cf7edbab150c1a8c3168b5bfd4a7
Luke <luke@clashproject.com>
2019-09-26 22:40:24 +0200

    Use numpy array instead of list comprehension

--- mlp/textGenerator.py
+++ mlp/textGenerator.py
@@ -17,28 +17,31 @@ class generator():
   def inpGenerator(self):
     n = 0
     tmpOut = np.zeros((self.batchsize,1),dtype=np.float32)
+    # Using lists instead of numpy arrays is about seven times slower
     if self.indexIn:
+      
+      tmpOut = np.zeros((self.batchsize,1),dtype=np.float32)
       tmpIn = [None]*self.batchsize
-      tmpIn[0] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
+      tmpIn[0][:] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
       tmpOut[0][0] = self.charDict[self.txt[self.inputs]] 
       while True:
-        for b in range(1,self.batchsize):
-          tmpIn[b] = tmpIn[b-1][1:]+[self.charDictList[self.txt[self.inputs+b]]]
+        for b in range(self.batchsize):
+          tmpIn[b][:] = np.append(tmpIn[b-1][1:],self.charDictList[self.txt[self.inputs+b]])
           tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
         for b in range(self.batchsize):
-          yield (np.array(tmpIn[b]), tmpOut[b])
+          yield (tmpIn[b], tmpOut[b])
         if n >= self.txtLen:
           n = 0
     else:
       tmpIn = [None]*self.batchsize
-      tmpIn[0] = list(itertools.chain.from_iterable(
+      tmpIn[0][:] = list(itertools.chain.from_iterable(
           [self.charDictList[self.txt[j]] for j in range(self.inputs)]))
       tmpOut[0][0] = self.charDict[self.txt[self.inputs]] 
       while True:
-        for b in range(1,self.batchsize):
-          tmpIn[b] = tmpIn[b-1][self.classes:]+self.charDictList[self.txt[self.inputs+b]]
+        for b in range(self.batchsize):
+          tmpIn[b][:] = np.append(tmpIn[b-1][self.classes:],self.charDictList[self.txt[self.inputs+b]])
           tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
         for b in range(self.batchsize):
-          yield (np.array(tmpIn[b]), tmpOut[b])
+          yield (tmpIn[b], tmpOut[b])
         if n >= self.txtLen:
           n = 0

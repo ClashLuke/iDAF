90acb0eaa52e82c5e0b003ed6cc335dfe1fe69a7
Luke <luke@clashproject.com>
2019-09-26 22:25:37 +0200

    Use batching, use python-lists instead of numpy arrays

--- mlp/textGenerator.py
+++ mlp/textGenerator.py
@@ -5,7 +5,7 @@ class generator():
   def __init__(self, batchsize, txt, outputs, indexIn, inputs, steps, charDictList, charDict, classes, changePerKerasEpoch):
     self.batchsize = batchsize
     self.txt = txt
-    self.txtLen = len(txt)-inputs-2
+    self.txtLen = len(txt)-inputs-batchsize-2
     self.outputs = outputs
     self.indexIn = indexIn
     self.inputs = inputs
@@ -15,32 +15,32 @@ class generator():
     self.classes = classes
     self.changePerKerasEpoch = changePerKerasEpoch
   def inpGenerator(self):
-    inputsTimesClasses = self.inputs*self.classes
     n = 0
-    if self.outputs == 1:
-      tmpOut = np.zeros((1),dtype=np.float32)
-      if self.indexIn:
-        tmpIn = np.zeros((self.inputs),dtype=np.float32)
-        tmpIn[:] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
-        tmpOut[0] = self.charDict[self.txt[self.inputs]] 
-        yield (tmpIn, tmpOut)
-        while True:
-          tmpIn[:] = np.append(tmpIn[1:],self.charDictList[self.txt[self.inputs+n]])
-          tmpOut[0] = self.charDict[self.txt[self.inputs+1+n]]
-          n+=1
-          yield (tmpIn, tmpOut)
-          if n >= self.txtLen:
-            n = 0
-      else:
-        tmpIn = np.zeros((inputsTimesClasses),dtype=np.float32)
-        tmpIn[:] = list(itertools.chain.from_iterable(
-            [self.charDictList[self.txt[j]] for j in range(self.inputs)]))
-        tmpOut[0] = self.charDict[self.txt[self.inputs]] 
-        yield (tmpIn, tmpOut)
-        while True:
-          tmpIn[:] = np.append(tmpIn[self.classes:],self.charDictList[self.txt[self.inputs+n]])
-          tmpOut[0] = self.charDict[self.txt[self.inputs+1+n]]
-          n+=1
-          yield (tmpIn, tmpOut)
-          if n >= self.txtLen:
-            n = 0
+    tmpOut = np.zeros((1),dtype=np.float32)
+    if self.indexIn:
+      tmpIn = [None]*self.batchsize
+      tmpIn[0] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
+      tmpOut[0] = self.charDict[self.txt[self.inputs]] 
+      yield (np.array(tmpIn), np.array(tmpOut))
+      while True:
+        for b in range(1,self.batchsize):
+          tmpIn[b] = tmpIn[b-1][1:]+[self.charDictList[self.txt[self.inputs+b]]]
+          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
+        for b in range(self.batchsize):
+          yield (np.array(tmpIn[b]), np.array(tmpOut[b]))
+        if n >= self.txtLen:
+          n = 0
+    else:
+      tmpIn = [None]*self.batchsize
+      tmpIn[0] = list(itertools.chain.from_iterable(
+          [self.charDictList[self.txt[j]] for j in range(self.inputs)]))
+      tmpOut[0] = self.charDict[self.txt[self.inputs]] 
+      yield (np.array(tmpIn), np.array(tmpOut))
+      while True:
+        for b in range(1,self.batchsize):
+          tmpIn[b] = tmpIn[b-1][1:]+[self.charDictList[self.txt[self.inputs+b]]]
+          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
+        for b in range(self.batchsize):
+          yield (np.array(tmpIn[b]), np.array(tmpOut[b]))
+        if n >= self.txtLen:
+          n = 0

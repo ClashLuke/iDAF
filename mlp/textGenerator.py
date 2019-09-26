6803f137fd29f30d8c194f3effaf6d9a39856ec9
Luke <luke@clashproject.com>
2019-09-26 23:32:58 +0200

    Iterate over dataset
    instead of iterating over the initial chunk many times

--- mlp/textGenerator.py
+++ mlp/textGenerator.py
@@ -20,26 +20,28 @@ class generator():
     # Using lists instead of numpy arrays is about seven times slower
     if self.indexIn:
       tmpIn = np.zeros((self.batchsize,self.inputs),dtype=np.float32)
-      tmpIn[0][:] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
+      tmpIn[-1][:] = [self.charDictList[self.txt[j]] for j in range(self.inputs)]
       tmpOut[0][0] = self.charDict[self.txt[self.inputs]] 
       while True:
         for b in range(self.batchsize):
-          tmpIn[b][:] = np.append(tmpIn[b-1][1:],self.charDictList[self.txt[self.inputs+b]])
-          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
+          tmpIn[b][:] = np.append(tmpIn[b-1][1:],self.charDictList[self.txt[self.inputs+b+n]])
+          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b+n]]
         for b in range(self.batchsize):
           yield (tmpIn[b], tmpOut[b])
+        n+=self.batchsize
         if n >= self.txtLen:
           n = 0
     else:
       tmpIn = np.zeros((self.batchsize,self.inputs*self.classes),dtype=np.float32)
-      tmpIn[0][:] = list(itertools.chain.from_iterable(
+      tmpIn[-1][:] = list(itertools.chain.from_iterable(
           [self.charDictList[self.txt[j]] for j in range(self.inputs)]))
       tmpOut[0][0] = self.charDict[self.txt[self.inputs]] 
       while True:
         for b in range(self.batchsize):
-          tmpIn[b][:] = np.append(tmpIn[b-1][self.classes:],self.charDictList[self.txt[self.inputs+b]])
-          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b]]
+          tmpIn[b][:] = np.append(tmpIn[b-1][self.classes:],self.charDictList[self.txt[self.inputs+b+n]])
+          tmpOut[b][0] = self.charDict[self.txt[self.inputs+1+b+n]]
         for b in range(self.batchsize):
           yield (tmpIn[b], tmpOut[b])
+        n+=self.batchsize
         if n >= self.txtLen:
           n = 0

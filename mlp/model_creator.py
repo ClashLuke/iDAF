9cc06d0f31a8476a6fa3e9e31e788bd428d31016
Lucas Nestler <lucasnestler@web.de>
2020-04-15 22:04:54 +0200

    feat: improve imports

--- mlp/model_creator.py
+++ mlp/model_creator.py
@@ -1,28 +1,30 @@
-import tensorflow as tf
-import tensorflow_addons as tfa
+from tensorflow.keras import Model
+from tensorflow.keras.initializers import orthogonal as initializer
+from tensorflow.keras.layers import (Add, BatchNormalization, Concatenate, Dense,
+                                     Embedding, Flatten, GaussianDropout, Input,
+                                     Multiply, Softmax)
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import plot_model
+from tensorflow_addons.layers import GELU
+from tensorflow_addons.optimizers import LAMB as OPTIMIZER
 
 from . import utils
 
 
 def add_advanced_layers(layer, leaky_relu, batch_norm):
     if batch_norm:
-        layer = tf.keras.layers.BatchNormalization()(layer)
-    if leaky_relu:
-        layer = tf.keras.layers.LeakyReLU(0.2)(layer)
+        layer = BatchNormalization()(layer)
     return layer
 
 
 def get_initial_binary_layer(initial_lstm, gpu, bidirectional,
                              inputs, unroll, classes, dropout,
                              two_dimensional, embedding):
-    if embedding:
-        inp = tf.keras.layers.Input(shape=(inputs,))
-        layer = tf.keras.layers.Embedding(input_dim=inputs, output_dim=classes)(inp)
-    else:
-        inp = tf.keras.layers.Input(shape=(inputs * classes,))
-        layer = inp
+    inp = Input(shape=(inputs,))
+    layer = Embedding(input_dim=inputs, output_dim=classes)(inp)
+    layer = Flatten()(layer)
     if dropout:
-        layer = tf.keras.layers.GaussianDropout(dropout)(layer)
+        layer = GaussianDropout(dropout)(layer)
     return layer, inp
 
 
@@ -34,72 +36,54 @@ def initialise_list(length, init_value, differing_value_position, differing_valu
 
 def get_input_layer(layer, inputs, classes, input_dense):
     if input_dense:
-        layer = tf.keras.layers.Dense(units=inputs,
-                                      kernel_initializer=tf.keras.initializers.Orthogonal())(
-                layer)
-        layer = tfa.layers.GELU()(layer)
+        layer = Dense(units=inputs, kernel_initializer=initializer())(layer)
+        layer = GELU()(layer)
     return layer
 
 
 def get_hidden_layers(layer, layer_count, neuron_list, leaky_relu, batch_norm,
-                      concat_dense,
-                      two_dimensional, dropout, depth):
-    if two_dimensional:
-        def dense(*args, **kwargs):
-            return tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(*args,
-                                                                         **kwargs))
-    else:
-        def dense(*args, **kwargs):
-            return tf.keras.layers.Dense(*args, **kwargs)
+                      concat_dense, two_dimensional, dropout, depth):
     for i in range(layer_count - 1):
         n = neuron_list[i]
         prev_in = layer
         for _ in range(depth):
-            key_layer = dense(n, kernel_initializer=tf.keras.initializers.Orthogonal())(
+            key_layer = Dense(n, kernel_initializer=initializer())(
                     layer)
-            query_layer = dense(n,
-                                kernel_initializer=tf.keras.initializers.Orthogonal())(
+            query_layer = Dense(n, kernel_initializer=initializer())(
                     layer)
-            value_layer = tfa.layers.GELU()(key_layer)
-            value_layer = dense(n,
-                                kernel_initializer=tf.keras.initializers.Orthogonal())(
+            value_layer = GELU()(key_layer)
+            value_layer = Dense(n, kernel_initializer=initializer())(
                     value_layer)
-            value_layer = tf.keras.layers.Softmax()(value_layer)
-            key_layer = tf.keras.layers.Multiply()([key_layer, value_layer])
-            layer = tf.keras.layers.Add()([query_layer, key_layer])
-            layer = tf.keras.layers.BatchNormalization(axis=1)(layer)
-            layer = tf.keras.layers.GaussianDropout(dropout)(layer)
-            layer = tfa.layers.GELU()(layer)
+            value_layer = Softmax()(value_layer)
+            key_layer = Multiply()([key_layer, value_layer])
+            layer = Add()([query_layer, key_layer])
+            layer = BatchNormalization(axis=1)(layer)
+            layer = GaussianDropout(dropout)(layer)
+            layer = GELU()(layer)
         if concat_dense:
-            layer = tf.keras.layers.Concatenate(axis=-1)([prev_in, layer])
+            layer = Concatenate(axis=-1)([prev_in, layer])
     return layer
 
 
 def get_output(layer, concat_before_output, outputs, classes, output_activation, loss,
                two_dimensional):
-    if two_dimensional:
-        layer = tf.keras.layers.Flatten()(layer)
     if 'crossentropy' not in loss:
         classes = 1
-    layer = tf.keras.layers.Dense(units=outputs * classes, activation=output_activation,
-                                  kernel_initializer=tf.keras.initializers.Orthogonal())(
-            layer)
-
-    if outputs > 1:
-        layer = tf.keras.layers.Reshape((outputs, classes))(layer)
+    layer = Dense(units=outputs * classes, activation=output_activation,
+                  kernel_initializer=initializer())(layer)
     return layer
 
 
 def compile_model(inp, layer, learning_rate, draw_model, loss, metric, model_compile):
-    model = tf.keras.Model(inputs=[inp], outputs=[layer])
+    model = Model(inputs=[inp], outputs=[layer])
     if model_compile:
         model.compile(loss=loss,
-                      optimizer=tfa.optimizers.LAMB(lr=learning_rate,
-                                                    weight_decay_rate=1e-3),
+                      optimizer=OPTIMIZER(lr=learning_rate,
+                                          weight_decay_rate=1e-3),
                       metrics=[metric])
         model.summary()
         if draw_model:
-            tf.keras.utils.plot_model(model, to_file='model.png')
+            plot_model(model, to_file='model.png')
     return model
 
 
@@ -127,21 +111,17 @@ def get_model(leakyRelu=True, batchNorm=True, trainNewModel=True,
     if trainNewModel:
         # Input layer
         if indexIn:
-            inp = tf.keras.layers.Input(shape=(inputs,))
-            layer = tf.keras.layers.GaussianDropout(dropout)(inp)
+            inp = Input(shape=(inputs,))
+            layer = GaussianDropout(dropout)(inp)
         else:
             layer, inp = get_initial_binary_layer(initialLSTM, gpu, bidirectional,
                                                   inputs,
                                                   unroll, classes, inputDense,
                                                   twoDimensional, embedding)
-        layer = tf.keras.layers.Dense(inputs,
-                                      kernel_initializer=tf.keras.initializers.Orthogonal())(
-                layer)
-        layer = tf.keras.layers.BatchNormalization()(layer)
-        layer = tfa.layers.GELU()(layer)
         layer = get_hidden_layers(layer, layerCount, neuronList, leakyRelu, batchNorm,
                                   concatDense, twoDimensional, dropout, depth)
-        layer = get_output(layer, concatBeforeOutput, outputs, classes, outputActivation,
+        layer = get_output(layer, concatBeforeOutput, outputs, classes,
+                           outputActivation,
                            loss, twoDimensional)
         # Compiling and displaying model
         model = compile_model(inp, layer, learningRate, drawModel, loss, metric,
@@ -149,6 +129,6 @@ def get_model(leakyRelu=True, batchNorm=True, trainNewModel=True,
     else:
         utils.get_previous_weights_from_gdrive(weightFolderName)
         last_used_model = utils.get_latest_model_name(weightFolderName)
-        model = tf.keras.models.load_model(last_used_model)
+        model = load_model(last_used_model)
         model.summary()
     return model

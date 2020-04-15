7b543a2b8821b53e936f58c6af3fa29d39ab85ee
Lucas Nestler <lucasnestler@web.de>
2020-04-16 00:29:33 +0200

    fix: move average from input to output

--- mlp/model_creator.py
+++ mlp/model_creator.py
@@ -23,7 +23,7 @@ def get_initial_binary_layer(initial_lstm, gpu, bidirectional,
     inp = Input(shape=(inputs,))
     layer = Embedding(input_dim=256, output_dim=classes)(inp)
     if not class_neurons:
-        layer = Average()(layer)
+        layer = Flatten()(layer)
     if dropout:
         layer = GaussianDropout(dropout)(layer)
     return layer, inp
@@ -68,7 +68,7 @@ def get_output(layer, concat_before_output, outputs, classes, output_activation,
     if 'crossentropy' not in loss:
         classes = 1
     if class_neurons:
-        layer = Flatten()(layer)
+        layer = Average()(layer)
     layer = Dense(units=256, activation=output_activation,
                   kernel_initializer=initializer())(layer)
     return layer
